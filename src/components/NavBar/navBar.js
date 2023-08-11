0b470c41bf62c3a498370ed3cefa3441df22f864
jatdalf <70412644+jatdalf@users.noreply.github.com>
2023-08-10 22:43:42 -0300

    lamding

--- src/components/NavBar/navBar.js
+++ src/components/NavBar/navBar.js
@@ -2,7 +2,10 @@ import { Link } from "react-router-dom";
 import style from "./navBar.module.css"
 
 const navBar = ()=>{
-    
+    return (
+        
+    )
+    <a>This is a navBar</a>
 }
 
 export default navBar
