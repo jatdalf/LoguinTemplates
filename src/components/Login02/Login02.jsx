b13956fa63d5b5a476aec0fad0330618bcd68cbf
jatdalf <70412644+jatdalf@users.noreply.github.com>
2023-08-20 20:53:59 -0300

    login 01

--- src/components/Login02/Login02.jsx
+++ src/components/Login02/Login02.jsx
@@ -4,30 +4,28 @@ import LogoImg from "../../assets/OcasaLogoSmall.png"
 
 const Loguin001 = () => {
   return (
-  <div class={style.Login2_body}>
+  <div class={style.centrado}>
+    <div class={style.align_Center}>
+      <div class={style.register}>
+      <img src={LogoImg} className={style.spinhov3D} alt="Logo small Ocasa" />
+        <h2>Welcome</h2>
+        <form action="" method="post" class="form">
+          <div class={style.register}>
+            <input type="email" placeholder="user@mailaddress.com"/>
+          </div>
+          <div class={style.register}>
+            <input type="password" placeholder="*************"/>
+          </div>
 
-  <div class={style.align_item}>
-    <div class={style.register}>
-    <img src={LogoImg} className={style.spinhov3D} alt="Logo small Ocasa" />
-      <h2>Welcome</h2>
-      <form action="" method="post" class="form">
-        <div class={style.register}>
-          <input type="email" placeholder="user@mailaddress.com"/>
-        </div>
+          <div class={style.register_submit}>
+            <input type="submit" value="Log In"/>
+          </div>
 
-        <div class={style.form__field}>
-          <imput type="password" placeholder="••••••••••••"/>
-        </div>
-
-        <div class={style.form__field}>
-          <input type="submit" value="Log In"/>
-        </div>
-
-      </form>
-      <p>enter username and password</p>
+        </form>
+        <p>Enter username and password</p>
+      </div>
     </div>
   </div>
-</div>
   )
 }
 
