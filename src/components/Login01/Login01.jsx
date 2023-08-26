555371553942a2564fa839c8671ae75dc50d5531
jatdalf <70412644+jatdalf@users.noreply.github.com>
2023-08-26 00:15:01 -0300

    login 01

--- src/components/Login01/Login01.jsx
+++ src/components/Login01/Login01.jsx
@@ -15,25 +15,25 @@ from 'mdb-react-ui-kit';
 
 const Loguin001 = () => {
   return (
-    <MDBContainer fluid>
-      <MDBRow className='d-flex justify-content-center align-items-center h-100'>
+    <MDBContainer fluid >
+      <MDBRow className={style.centrado}>
         <MDBCol col='12'>
-          <MDBCard className='bg-white my-5 mx-auto' style={{borderRadius: '1rem', maxWidth: '500px'}}>
-            <MDBCardBody className='p-5 w-100 d-flex flex-column'>
-              <h2 className="fw-bold mb-2 text-center">Sign in</h2>
-              <p className="text-white-50 mb-3">Please enter your login and password!</p>
+          <MDBCard>
+            <MDBCardBody >
+              <h2 className={style.l1H2}>Sign in</h2>
+              <p >Please enter your login and password!</p>
 
               <div className={style.BgLogin1}>
                 <img src={bgwh} className={style.stretch} alt="warehouse clean and arranged" />
               </div>
 
-              <MDBInput wrapperClass='mb-4 w-100' label='Email address' id='formControlLg' type='email' size="lg"/>
-              <MDBInput wrapperClass='mb-4 w-100' label='Password' id='formControlLg' type='password' size="lg"/>
-              <MDBCheckbox name='flexCheck' id='flexCheckDefault' className='mb-4' label='Remember password' />
-              <MDBBtn size='lg'>
+              <MDBInput className={style.l1imput} label='Email address' id='formControlLg' type='email' size="lg"/>
+              <MDBInput className={style.l1imput} label='Password' id='formControlLg' type='password' size="lg"/>
+              <MDBCheckbox name='flexCheck' id='flexCheckDefault' className={style.l1chkbox} label='Remember me' />
+              <MDBBtn className={style.l1btn} size='lg' >
                 Login
               </MDBBtn>
-              <hr className="my-4" />
+              <hr className={style.l1hr}/>
 
             </MDBCardBody>
           </MDBCard>
