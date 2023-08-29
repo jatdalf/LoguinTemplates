34578b7a8e97f821687f7e456f14c54a0cc6a4b8
jatdalf <70412644+jatdalf@users.noreply.github.com>
2023-08-29 20:47:18 -0300

    login 03

---
+++ src/assets/carrouselData.js
@@ -0,0 +1,22 @@
+export const carrouselData = [
+    {
+        id: 1,
+        imgUrl: "src/assets/wh1.jpg"
+    },
+    {
+        id: 2,
+        imgUrl: "src/assets/wh2.jpg"
+    },
+    {
+        id: 3,
+        imgUrl: "src/assets/truck.jpg"
+    },
+    {
+        id: 4,
+        imgUrl: "src/assets/wh3.jpg"
+    },
+    {
+        id: 5,
+        imgUrl: "src/assets/wh4.jpg"
+    }
+];
