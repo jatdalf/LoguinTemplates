44aca2d92be19d4789589bbb427aa01eef74b79b
jatdalf <70412644+jatdalf@users.noreply.github.com>
2023-08-30 23:49:55 -0300

    login 03

--- src/components/Login03/Login03.jsx
+++ src/components/Login03/Login03.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import style from "./Login03.module.css"
 
+
 const Loguin001 = () => {
   return (
-    <div>
-      Login03
+    <div className={style.mainContainer}>
+      <div className={style.sliderContainer}>
+        <div className={style.imagesContainer}>
+
+          Login03
+        </div>
+      </div>
     </div>
   )
 }
