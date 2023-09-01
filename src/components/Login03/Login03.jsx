b16f24e082cf986c8897146df76f8d4dca7f5648
jatdalf <70412644+jatdalf@users.noreply.github.com>
2023-08-31 23:05:37 -0300

    login 03

--- src/components/Login03/Login03.jsx
+++ src/components/Login03/Login03.jsx
@@ -1,14 +1,45 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import style from "./Login03.module.css"
-
+import { carrouselData } from '../../assets/carrouselData';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 const Loguin001 = () => {
+  const imgRef = useRef();
+  const [currentImgIndex, setCurrentImgIndex] = useState(0);
+  useEffect(() =>{
+    const listNode = imgRef.current;
+    const imageNode = listNode.querySelectorAll("li > img")[currentImgIndex]
+    if (imageNode){
+      imageNode.scrollIntoView({
+        behavior: "smooth"
+      })
+    }
+  },[currentImgIndex])
+
+  const goToImg=(imgIdx) =>{setCurrentImgIndex(imgIdx)}
+
   return (
     <div className={style.mainContainer}>
       <div className={style.sliderContainer}>
         <div className={style.imagesContainer}>
-
-          Login03
+          <ul ref={imgRef}>
+            {
+              carrouselData.map((image) =>{
+                return <li key={image.id}>
+                  <img src={image.imgUrl} className={style.imageCss} />
+                </li>
+              })
+            }
+          </ul>
+        </div>
+        <div className={style.dotsContainer}>
+          {
+            carrouselData.map((_, idx) =>(
+              <div key={idx} className={style.dotCss}
+                onClick={()=>goToImg(idx)}>&#9864;</div>
+            ))            
+          }
         </div>
       </div>
     </div>
