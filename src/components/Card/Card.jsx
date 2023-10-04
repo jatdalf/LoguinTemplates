dbe1db1076c8e85d519f1e3f31a7b3d5c5ff20c3
jatdalf <70412644+jatdalf@users.noreply.github.com>
2023-10-04 18:23:30 -0300

    cards

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,11 +3,15 @@ import {Link} from 'react-router-dom'
 
 const Card = (props)=>{    
     const data = props
+    console.log(props.cardImg)
+    console.log(props.name)
+    console.log(props.cardAdress)
+
     return (
-    <div className={style.card}>   card  
+    <div className={style.cardEliminarEstaFrase}>     
         <Link to={`/${props.cardAdress}`}>
-        {/* <div>
-            <img className={style.FlagImg} src={props.flag_img} alt={props.flag_img}  width="150em" height="110em"/>
+        {/* <div> 
+            <img className={style.FlagImg} src={props.cardImg} alt={props.cardImg}  width="150em" height="110em"/>
             <h3>{props.name}</h3>
         </div> */}
         </Link>    
