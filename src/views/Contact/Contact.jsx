71c02fb49cc06892e6d74068799c620bc0f778da
jatdalf <70412644+jatdalf@users.noreply.github.com>
2023-10-07 17:53:57 -0300

    contacto

--- src/views/Contact/Contact.jsx
+++ src/views/Contact/Contact.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import style from "./Contact.module.css"
+import React from 'react';
+import style from './Contact.module.css';
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div className={style.contact}>
+      <h2>Contacto</h2>
+      <p>Correo Electrónico: <a href="mailto:jorgetoso@hotmail.com">jorgetoso@hotmail.com</a></p>
+      <p>Teléfono: +543516207428</p>
+      <p>LinkedIn: <a href="https://www.linkedin.com/in/jorgetoso/" target="_blank" rel="noopener noreferrer">https://www.linkedin.com/in/jorgetoso/</a></p>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
+
+
+
+
