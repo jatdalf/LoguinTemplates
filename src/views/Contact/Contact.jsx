4e3a13ce927f07e692fd81c0e7a94aeb8c1226dd
jatdalf <70412644+jatdalf@users.noreply.github.com>
2023-10-10 15:55:14 -0300

    conact me

--- src/views/Contact/Contact.jsx
+++ src/views/Contact/Contact.jsx
@@ -4,9 +4,9 @@ import style from './Contact.module.css';
 const Contact = () => {
   return (
     <div className={style.contact}>
-      <h2>Contacto</h2>
-      <p>Correo Electrónico: <a href="mailto:jorgetoso@hotmail.com">jorgetoso@hotmail.com</a></p>
-      <p>Teléfono: +543516207428</p>
+      <h2>Contact</h2>
+      <p>E-mail: <a href="mailto:jorgetoso@hotmail.com">jorgetoso@hotmail.com</a></p>
+      <p>Telephone number: +543516207428</p>
       <p>LinkedIn: <a href="https://www.linkedin.com/in/jorgetoso/" target="_blank" rel="noopener noreferrer">https://www.linkedin.com/in/jorgetoso/</a></p>
     </div>
   );
