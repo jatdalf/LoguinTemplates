fc3e37a2d99eb2c9a875ea1527d5bedb5c8fdc6f
jatdalf <70412644+jatdalf@users.noreply.github.com>
2023-10-12 08:14:47 -0300

    About me

--- src/views/About/About.jsx
+++ src/views/About/About.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import style from "./About.module.css"
+import React from 'react';
+import style from './About.module.css';
+import mypic1 from '../../assets/MyPic1.jpg';
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className={style.aboutContainer}>
+      <div className={style.aboutContent}>
+        <h1 className={style.aboutH1}>Hi!, I am</h1>
+        <h1 className={style.myName}>Jorge</h1>
+        <img src={mypic1} className={style.pic1} alt="Jorge's picture" />
+        <p className={style.aboutP1}>Scroll through your area of interest to learn more about me</p>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
