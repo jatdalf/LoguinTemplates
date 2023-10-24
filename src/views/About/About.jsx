4cbe5f377c326048949773f28fe8b1d558574b24
jatdalf <70412644+jatdalf@users.noreply.github.com>
2023-10-24 12:41:11 -0300

    About me

--- src/views/About/About.jsx
+++ src/views/About/About.jsx
@@ -17,17 +17,53 @@ const About = () => {
       <div className={style.banner}>
         <div className={style.bannerItem}>          
           <p className={style.bannerTitle}>Dev Skills</p>
-          <p className={style.bannerP1}>Full Stack Developer<br/> 
+          <p className={style.bannerP1}><br/><br/><br/>Full Stack Web Developer<br/> 
                                         with skills in React, Javascript, Css<br/>
                                         Express, Sequelize, NodeJS, PostgreSql<br/>
                                         BootsTrap/Chakra, Github, VueJs  <br/>
                                         React Native.
                                         </p>
         </div>
-        <div className={style.bannerItem}><p className={style.bannerTitle}>Studies</p></div>
-        <div className={style.bannerItem}><p className={style.bannerTitle}>Languages</p></div>
-        <div className={style.bannerItem}><p className={style.bannerTitle}>Job history</p></div>
-        <div className={style.bannerItem}><p className={style.bannerTitle}>Hobbies</p></div>
+        <div className={style.bannerItem}><p className={style.bannerTitle}>Studies</p>
+          <p className={style.bannerP1}>Full Stack Developer<br/> 
+                                      "Soy Henry" Bootcamp.<br/> 700 hours of study<br/> 
+                                      theory, individual and group practice <br/><br/>
+                                      Computer systems analyst<br/>
+                                      IES21ST Century University<br/>
+                                      Desktop and web App dev<br /><br />
+                                      Virtual Simulations and Videogames<br />
+                                      IES21ST Century University<br/>
+                                      </p>
+        </div>
+        <div className={style.bannerItem}><p className={style.bannerTitle}>Languages</p>
+        <p className={style.bannerP1}><br/><br/>English<br/> 
+                                      C2 Proficent Efset Certificate<br/><br/> 
+                                      Portuguese<br/>
+                                      Basic, 1 year training<br/><br/>
+                                      Spanish<br/>
+                                      Native language<br />                                      
+                                      </p>
+        </div>
+        <div className={style.bannerItem}><p className={style.bannerTitle}>Experience</p>
+        <p className={style.bannerP1}>(ACADEMIC) Full Stack Web Dev<br/> 
+                                      Soy Henry <a href='https://www.soyhenry.com/'>(www.soyhenry.com)</a><br/>
+                                      Development of a Single Page App<br/>
+                                      Consuming Api and using Database<br/><hr /> 
+                                      Development of a website marketplace,<br/>
+                                      with payment gateway and authentication,<br/> 
+                                      working with a multinational team<br/> 
+                                      applying agile technologies<br/>
+                                      <br/><br />  
+                                                                          
+                                      </p>
+        </div>
+        <div className={style.bannerItem}>
+          <p className={style.bannerTitle}>Hobbies</p>
+            <p className={style.bannerP1}>Love to spend time with family<br/>
+                                          and friends<br/>
+
+          </p>
+        </div>
       </div>
     </div>
   );
