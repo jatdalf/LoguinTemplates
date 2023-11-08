a706e0cc3553064e4687b7746b8b3deec5e8614c
jatdalf <70412644+jatdalf@users.noreply.github.com>
2023-11-08 11:27:34 -0300

    whatsapp button

--- src/components/Whatsapp/Whatsapp.jsx
+++ src/components/Whatsapp/Whatsapp.jsx
@@ -1,14 +1,15 @@
 import React from "react";
-import wspIcon from '../../assets/WhatsApp-Logo.wine.svg'
-import style from './Whatsapp.module.css'
+import style from './Whatsapp.module.css';
+import wspIcon from '../../assets/WhatsApp-Logo.wine.svg';
 
 const WhatsApp = () => {
     return (
-        <a href='https://api.whatsapp.com/send?phone=5496207428' className={style.btnwsp}
-        target='_blank' >
-            <img source={wspIcon} className={style.iconwhatsapp}></img>
+        <a href='https://api.whatsapp.com/send?phone=5493516207428' target='_blank'>
+            <div className={style.iconContent}>
+                <img className={style.iconwhatsapp} src={wspIcon} alt="WhatsApp Icon" />
+            </div>
         </a>
-           );
+    );
 };
 
 export default WhatsApp;
--- src/views/Contact/Contact.jsx
+++ src/views/Contact/Contact.jsx
@@ -8,14 +8,13 @@ const Contact = () => {
     <div>
       <div className={style.contact}>
         <h2 className={style.contactH2}>Contact</h2>
-        <p>E-mail: <a href="mailto:jorgetoso@hotmail.com">jorgetoso@hotmail.com</a></p>
+        <p>E-mail: <a className={style.contacta} href="mailto:jorgetoso@hotmail.com">jorgetoso@hotmail.com</a></p>
         <p>Telephone number: +543516207428</p>
-        <p>LinkedIn: <a href="https://www.linkedin.com/in/jorgetoso/" target="_blank" rel="noopener noreferrer">https://www.linkedin.com/in/jorgetoso/</a></p>
+        <p>LinkedIn: <a className={style.contacta} href="https://www.linkedin.com/in/jorgetoso/" target="_blank" rel="noopener noreferrer">https://www.linkedin.com/in/jorgetoso/</a></p>
       </div>
       <div className={style.svgContainer}>
         <Whatsapp />
-        <img src={scLogo} className={style.svg}></img>
-       
+        <img src={scLogo} className={style.svg}></img>       
       </div>
     </div>
   );
